d02abc445ee00af21f306454221382c17f65abf0
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-10-17 10:17:37 +0700

    delete defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { defineConfig, passthroughImageService } from "astro/config";
+import { passthroughImageService } from "astro/config";
 
 import react from "@astrojs/react";
 
