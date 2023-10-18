30f372a4a6d1b256eaffb162e06b04a0ca7b579d
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-10-18 10:01:12 +0700

    change icon button on decision section

--- src/modules/home/sections/Decisions/Button.tsx
+++ src/modules/home/sections/Decisions/Button.tsx
@@ -1,8 +1,8 @@
-import { BsArrowRight } from "react-icons/bs";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function Button() {
     return (
-        <button className="flex gap-4 mx-auto items-center bg-gray-400 text-white px-8 py-3 w-fit rounded-full">All Product Features <BsArrowRight size={25} /></button>
+        <button className="flex gap-4 mx-auto items-center bg-gray-400 text-white px-8 py-3 w-fit rounded-full">All Product Features <FaArrowRightLong size={20} /></button>
     )
 }
 
