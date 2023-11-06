66392fad2c88d090e550131b80831df758e7de40
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-06 15:43:33 +0700

    feat(enterprice): create cta section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 interface IProps {
     text: string;
-    theme?: "light" | "dark"
+    theme?: "light" | "normal" | "dark"
 }
 
-function Button({ text, theme = "dark" }: IProps) {
-    if (theme === "dark") {
+function Button({ text, theme = "normal" }: IProps) {
+    if (theme === "normal") {
         return (
             <button className="flex gap-4 items-center bg-gray-400 text-white text-sm px-8 py-3 w-fit rounded-full">{text}</button>
         )
     } else if (theme === "light") {
         return (
             <button className="flex gap-4 items-center bg-white text-black border border-gray-500 text-sm px-8 py-3 w-fit rounded-full">{text}</button>
+        )
+    } else if (theme === "dark") {
+        return (
+            <button className="flex gap-4 items-center bg-gray-600 text-white text-sm px-8 py-3 w-fit rounded-full">{text}</button>
         )
     }
 }
