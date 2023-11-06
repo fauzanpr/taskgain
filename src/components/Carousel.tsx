8e6df2b2ca1d515064b75aba57e9bb2046fd196a
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-06 15:06:57 +0700

    feat(enterprice): create review section

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,38 +1,77 @@
 import CarouselItem from "./CarouselItem"
 
-function Carousel() {
-    return (
-        <div id="controls-carousel" className="relative w-full" data-carousel="static">
-            {/* <!-- Carousel wrapper --> */}
-            <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <CarouselItem stars={4} author="Wouter H." position="Chapter Lead" review="I often have meetings where we bring together a large group of offline and online stakeholders. StoriesOnBoard has helped me structure this meeting while we are brainstorming on product design and the E2E process. The result is easily improved during follow-up meetings or exported to an Excel or pdf and shared within the organization." />
+interface IProps {
+    imagePosition?: "left" | "right";
+};
+
+function Carousel({ imagePosition = "right" }: IProps) {
+    if (imagePosition === "right") {
+        return (
+            <div id="controls-carousel" className="relative w-full" data-carousel="static">
+                {/* <!-- Carousel wrapper --> */}
+                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                        <CarouselItem stars={4} author="Wouter H." position="Chapter Lead" review="I often have meetings where we bring together a large group of offline and online stakeholders. StoriesOnBoard has helped me structure this meeting while we are brainstorming on product design and the E2E process. The result is easily improved during follow-up meetings or exported to an Excel or pdf and shared within the organization." imagePosition={imagePosition} />
+                    </div>
+                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                        <CarouselItem stars={5} author="Fauzan P." position="Ordinary Human" review="INI KEREN POL" imagePosition={imagePosition} />
+                    </div>
                 </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <CarouselItem stars={5} author="Fauzan P." position="Ordinary Human" review="INI KEREN POL" />
+                {/* <!-- Slider controls --> */}
+                <div className="flex absolute left-1/3 bottom-4 z-30">
+                    <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200  group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                            </svg>
+                            <span className="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span className="sr-only">Next</span>
+                        </span>
+                    </button>
                 </div>
             </div>
-            {/* <!-- Slider controls --> */}
-            <div className="flex absolute left-1/3 bottom-4 z-30">
-                <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200  group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                        <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
+        )
+    } else if (imagePosition === "left") {
+        return (
+            <div id="controls-carousel" className="relative w-full" data-carousel="static">
+                {/* <!-- Carousel wrapper --> */}
+                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                        <CarouselItem stars={4} author="Wouter H." position="Chapter Lead" review="I often have meetings where we bring together a large group of offline and online stakeholders. StoriesOnBoard has helped me structure this meeting while we are brainstorming on product design and the E2E process. The result is easily improved during follow-up meetings or exported to an Excel or pdf and shared within the organization." imagePosition={imagePosition} />
+                    </div>
+                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                        <CarouselItem stars={5} author="Fauzan P." position="Ordinary Human" review="INI KEREN POL" imagePosition={imagePosition} />
+                    </div>
+                </div>
+                {/* <!-- Slider controls --> */}
+                <div className="flex absolute right-0 bottom-4 z-30">
+                    <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200  group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                            </svg>
+                            <span className="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 group-hover:bg-gray-300 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span className="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Carousel
--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -5,24 +5,43 @@ interface IProps {
     author: string;
     position: string;
     stars: number;
+    imagePosition?: "right" | "left";
 }
 
-function CarouselItem({ review, author, position, stars }: IProps) {
-    return (
-        <div className="flex gap-8">
-            <div className="w-1/2 flex flex-col justify-between">
-                <div className="flex flex-col gap-4">
-                    <Star counter={stars} />
-                    <p className="text-lg italic">“{ review }”</p>
-                </div>
-                <div>
-                    <p>{ author }</p>
-                    <p className="text-gray-400 font-light text-sm">{ position }</p>
+function CarouselItem({ review, author, position, stars, imagePosition = "right" }: IProps) {
+    if (imagePosition === "left") {
+        return (
+            <div className="flex gap-8">
+                <div className="bg-gray-400 h-96 w-1/2 rounded-xl" />
+                <div className="w-1/2 flex flex-col justify-between">
+                    <div className="flex flex-col gap-4">
+                        <Star counter={stars} />
+                        <p className="text-lg italic">“{review}”</p>
+                    </div>
+                    <div>
+                        <p>{author}</p>
+                        <p className="text-gray-400 font-light text-sm">{position}</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-gray-400 h-96 w-1/2 rounded-xl" />
-        </div>
-    )
+        )
+    } else if (imagePosition === "right") {
+        return (
+            <div className="flex gap-8">
+                <div className="w-1/2 flex flex-col justify-between">
+                    <div className="flex flex-col gap-4">
+                        <Star counter={stars} />
+                        <p className="text-lg italic">“{review}”</p>
+                    </div>
+                    <div>
+                        <p>{author}</p>
+                        <p className="text-gray-400 font-light text-sm">{position}</p>
+                    </div>
+                </div>
+                <div className="bg-gray-400 h-96 w-1/2 rounded-xl" />
+            </div>
+        )
+    }
 }
 
 export default CarouselItem;
