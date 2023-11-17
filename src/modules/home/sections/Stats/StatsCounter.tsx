517baeb65144fcc548efc78cdba51bbd8d16c84b
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-17 13:45:30 +0700

    feat(homepage): add counter on stats

---
+++ src/modules/home/sections/Stats/StatsCounter.tsx
@@ -0,0 +1,17 @@
+import CountUp from "react-countup"
+
+interface IProps {
+    end: number
+};
+
+function StatsCounter({ end }: IProps) {
+    return (
+        <CountUp start={0} end={end} delay={0}>
+            {({ countUpRef }) => (
+                <p className="text-5xl font-semibold text-white mb-4"><span ref={countUpRef as React.RefObject<HTMLDivElement>} />K</p>
+            )}
+        </CountUp>
+    )
+}
+
+export default StatsCounter;
