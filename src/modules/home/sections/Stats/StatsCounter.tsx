dcd544ace02d160f9a76c03b30cb284b7826cd24
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-28 13:46:03 +0700

    feat(homepage): add responsive on stats section

--- src/modules/home/sections/Stats/StatsCounter.tsx
+++ src/modules/home/sections/Stats/StatsCounter.tsx
@@ -8,7 +8,7 @@ function StatsCounter({ end }: IProps) {
     return (
         <CountUp start={0} end={end} delay={0}>
             {({ countUpRef }) => (
-                <p className="text-5xl font-semibold text-white mb-4"><span ref={countUpRef as React.RefObject<HTMLDivElement>} />K</p>
+                <p className="text-3xl lg:text-5xl font-semibold text-white mb-4"><span ref={countUpRef as React.RefObject<HTMLDivElement>} />K</p>
             )}
         </CountUp>
     )
