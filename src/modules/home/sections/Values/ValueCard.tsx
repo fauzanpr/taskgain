7e46a871198f9a4bf0ce598424b7cf12a6671bca
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-28 13:05:50 +0700

    feat(homepage): add responsive on values section

--- src/modules/home/sections/Values/ValueCard.tsx
+++ src/modules/home/sections/Values/ValueCard.tsx
@@ -14,8 +14,8 @@ function ValueCard({ id, title, description, isActive, setImageActive, iconOn, i
         <div className={`flex items-start gap-4 p-8 border-l-4 hover:border-primary cursor-pointer hover:bg-gray-50 ${isActive ? "bg-gray-50 border-primary" : "border-l-white"}`} onClick={() => setImageActive(id)}>
             { isActive ? <img src={iconOn} alt={title} /> : <img src={iconOff} alt={title} /> }
             <div>
-                <h3 className="text-xl mb-4 font-semibold">{ title }</h3>
-                <p>{ description }</p>
+                <h3 className="text-lg xl:text-xl mb-4 font-semibold">{ title }</h3>
+                <p className="text-sm xl:text-base">{ description }</p>
             </div>
         </div>
     )
