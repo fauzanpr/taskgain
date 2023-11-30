03abf9d5dcd38f09b149556574ed4e2bad331844
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-11-30 23:04:12 +0700

    feat(product): add responsive on integrated section

--- src/modules/products/sections/integrated/IntegratedCard.tsx
+++ src/modules/products/sections/integrated/IntegratedCard.tsx
@@ -11,17 +11,17 @@ interface IProps {
 
 function IntegratedCard({ title, description, image, install, rate }: IProps) {
   return (
-    <div className="flex items-start gap-4 h-64 w-[450px] mx-4 p-6 bg-[#1B1B1B] border border-[#383838] rounded-xl">
+    <div className="flex items-start gap-4 h-56 lg:h-64 w-[450px] mx-4 p-6 bg-[#1B1B1B] border border-[#383838] rounded-xl">
       <img src={image} alt="Logo Slack" />
       <div className="text-gray-400 flex flex-col gap-4">
-        <p className="text-white text-lg font-semibold">{ title }</p>
-        <div className="flex gap-2">
+        <p className="text-white text-base lg:text-lg font-semibold">{ title }</p>
+        <div className="flex items-center gap-2">
           <img src={Star.src} alt="Bintang warna kuning" />
-          <p>{ rate } rate</p>
+          <p className="text-sm lg:text-base">{ rate } rate</p>
           <img src={DownloadIcon.src} alt="Icon download" />
-          <p>{ install } installs</p>
+          <p className="text-sm lg:text-base">{ install } installs</p>
         </div>
-        <p>{ description }</p>
+        <p className="text-sm lg:text-base">{ description }</p>
       </div>
     </div>
   )
