35735f1622b429a6c8f44565e64a3184b778d911
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-12-01 00:11:34 +0700

    feat(product): update font size on deepdive section

--- src/modules/products/sections/deepdive/DeepDiveCard.tsx
+++ src/modules/products/sections/deepdive/DeepDiveCard.tsx
@@ -32,9 +32,9 @@ function DeepDiveCard({ title, description, reader, color }: IProps) {
         <div className="flex flex-col gap-6 p-8 shadow-md">
             <div className="flex justify-between items-center">
                 <div className={`h-4 w-4 rounded-full ${col || "bg-gray-500"}`} />
-                <p className="text-gray-500 font-light">{reader}</p>
+                <p className="text-sm lg:text-base text-gray-500 font-light">{reader}</p>
             </div>
-            <h3 className="text-3xl font-semibold">{title}</h3>
+            <h3 className="text-xl lg:text-3xl font-semibold">{title}</h3>
             <div className="flex items-start justify-between gap-4">
                 <p className="text-gray-400 w-3/4">{description}</p>
                 <button className="transition-all border w-1/4 border-gray-500 px-6 rounded-full hover:bg-primary hover:border-primary hover:text-white flex justify-center items-center">
