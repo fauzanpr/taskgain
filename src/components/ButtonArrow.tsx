0911243bf5723c3046840f3a74bf8dfe0bbad016
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-12-08 13:22:57 +0700

    feat: add hero on pricing and resources

--- src/components/ButtonArrow.tsx
+++ src/components/ButtonArrow.tsx
@@ -1,14 +1,22 @@
 // import { BsArrowRight } from "react-icons/bs";
 import ArrowRight from "@assets/icon/arrow-right.svg";
+import ArrowRightUp from "@assets/icon/arrow-leftup.svg";
 
 interface IProps {
     text: string;
-}       
+    type?: "normal" | "rightup"
+}
 
-function ButtonArrow({ text }: IProps) {
-    return (
-        <button className="transition-all flex gap-4 items-center bg-primary hover:bg-primaryHover text-white text-sm px-8 py-3 w-fit rounded-full">{ text } <img src={ArrowRight.src} alt="Panah ke kanan" /></button>
-    )
+function ButtonArrow({ text, type = "normal" }: IProps) {
+    if (type === "normal") {
+        return (
+            <button className="transition-all flex gap-4 items-center bg-primary hover:bg-primaryHover text-white text-sm px-8 py-3 w-fit rounded-full">{text} <img src={ArrowRight.src} alt="Panah ke kanan" /></button>
+        )
+    } else if (type === "rightup") {
+        return (
+            <button className="transition-all flex gap-4 items-center bg-primary hover:bg-primaryHover text-white text-sm px-8 py-3 w-fit rounded-full">{text} <img src={ArrowRightUp.src} alt="Panah ke kanan" /></button>
+        )
+    }
 }
 
 export default ButtonArrow;
